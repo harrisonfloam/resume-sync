6acbea91eea9bf19d01ca1381f1897acf55d5217
Harrison Floam <harrisonfloam@gmail.com>
2024-01-04 13:09:59 -0500

    complete get_recently_modified_resumes

--- resume-sync/resume_sync.py
+++ resume-sync/resume_sync.py
@@ -4,6 +4,9 @@ Sync resumes from Google Drive to Dropbox in PDF form.
 
 
 import os
+import io
+import json
+from datetime import date, datetime, timedelta
 import dropbox
 from dropbox import DropboxOAuth2FlowNoRedirect
 from google.auth.transport.requests import Request
@@ -11,7 +14,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import json
+from googleapiclient.http import MediaIoBaseDownload
 
 def is_in_github_action():
     """Return True if executing in Github Action"""
@@ -83,7 +86,45 @@ def get_dropbox_instance():
                 print(f"An error occurred creating a Dropbox API instance: {error}")
                 exit(1)
 
+def get_recently_modified_resumes(drive_instance):
+    # Calculate one week ago date
+    one_week_ago = datetime.utcnow() - timedelta(days=7)
+    one_week_ago_str = one_week_ago.isoformat() + 'Z'  # Format for Google Drive API
 
+    try:
+        # Folder IDs
+        resume_folder_id = '1FQAXueyM20GCbD4g1fn4EZjV4XawAGkC'
+        targeted_folder_id = '1bXW8_I_x7__gu30XbmmqdftH_fNyst2-'
+        
+        query = f"(parents = '{resume_folder_id}' or parents = '{targeted_folder_id}') and " \
+                f"modifiedTime > '{one_week_ago_str}'and " \
+                f"mimeType != 'application/vnd.google-apps.folder'"
+
+        results = drive_instance.files().list(q=query, pageSize=10, fields="files(id, name, modifiedTime)").execute()
+        items = results.get("files", [])
+
+        if not items:
+            print("No files found in the past week.")
+        else:
+            print("Files modified in the past week:")
+            for item in items:
+                print(f"{item['name']} (ID: {item['id']}, Modified: {item['modifiedTime']})")
+                
+                request = drive_instance.files().export(fileId=item['id'], mimeType='application/pdf')
+                fh = io.BytesIO()
+                downloader = MediaIoBaseDownload(fh, request)
+                done = False
+                
+                while not done:
+                    status, done = downloader.next_chunk()
+                
+                fh.seek(0)
+                with open(f"temp_pdf/{item['name']}.pdf", 'wb') as f:
+                    f.write(fh.read())
+                    
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+    
 
 def sync():
     drive_instance = get_drive_instance()
@@ -92,24 +133,7 @@ def sync():
     # If filename is passed in, only get that file TODO: leave this blank for now
     
     # If not, get any files in /Resume or /Resume/Targeted changed in the past week
-    try:
-        # Call the Drive v3 API
-        results = (
-            drive_instance.files()
-            .list(q="mimeType='application/vnd.google-apps.folder'", pageSize=10)
-            .execute()
-        )
-        items = results.get("files", [])
-
-        if not items:
-            print("No files found.")
-            return
-        print("Files:")
-        for item in items:
-            print(f"{item['name']} ({item['id']})")
-    except HttpError as error:
-        # TODO(developer) - Handle errors from drive API.
-        print(f"An error occurred: {error}")
+    get_recently_modified_resumes(drive_instance)
     
     # Get PDFs - do we need to save them in the wdir temporarily?
     
